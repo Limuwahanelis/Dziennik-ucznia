476c2256eac839a64cebce58e85076b787793167
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-01-28 12:37:38 +0100

    Add controller for students grades

---
+++ DziennikUcznia/DziennikUcznia/Controllers/StudentGradesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DziennikUcznia.Data;
+using DziennikUcznia.Models;
+
+namespace DziennikUcznia.Controllers
+{
+    public class StudentGradesController : Controller
+    {
+        private readonly SchoolDbContext_SQLServer _context;
+
+        public StudentGradesController(SchoolDbContext_SQLServer context)
+        {
+            _context = context;
+        }
+
+        // GET: StudentGrades
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Grades.ToListAsync());
+        }
+
+        // GET: StudentGrades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // GET: StudentGrades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentGrades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Value,Type")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(grade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(grade);
+        }
+
+        // GET: StudentGrades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades.FindAsync(id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+            return View(grade);
+        }
+
+        // POST: StudentGrades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Value,Type")] Grade grade)
+        {
+            if (id != grade.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(grade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GradeExists(grade.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(grade);
+        }
+
+        // GET: StudentGrades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // POST: StudentGrades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var grade = await _context.Grades.FindAsync(id);
+            if (grade != null)
+            {
+                _context.Grades.Remove(grade);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GradeExists(int id)
+        {
+            return _context.Grades.Any(e => e.Id == id);
+        }
+    }
+}
